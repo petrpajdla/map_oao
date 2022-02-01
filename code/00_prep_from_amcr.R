320c6b12328b1fe6dddf20bcf705a79d11b0c8ed
petrpajdla <petr.pajdla@protonmail.com>
2022-02-01 16:47:24 +0100

    read files based on date stamp

--- code/00_prep_from_amcr.R
+++ code/00_prep_from_amcr.R
@@ -1,50 +1,59 @@
 # joining tables exported from AMCR
+# export: 
+#   - projekt
+#   - akce
+#   - pian
+#   - dokumentacni jednotka
 
 library(tidyverse)
 library(here)
 
 # read data ---------------------------------------------------------------
 
-proj <- read_delim(here("data/raw", "export_2021-07-30_projekt.csv"),
+datestamp <- list.files(here("data/raw"), pattern = "^export") %>% 
+  str_extract("\\d{4}-\\d{2}-\\d{2}") %>% 
+  lubridate::as_date() %>% 
+  max()
+
+proj <- read_delim(here("data/raw", 
+                        paste0("export_", datestamp, "_projekt.csv")),
                    delim = "#",
                    col_types = paste0(rep("c", 48), collapse = ""),
-                   quote = "")
+                   quote = "") %>% 
+  select(ident_cely, id_rok, stav_popis, typ_projektu, 
+         organizace_prihlaseni, starts_with("geometry"))
 
-akce <- read_delim(here("data/raw", "export_2021-07-30_akce.csv"), 
+akce <- read_delim(here("data/raw", 
+                        paste0("export_", datestamp, "_akce.csv")), 
                    delim = "#", 
                    col_types = paste0(rep("c", 40), collapse = ""), 
-                   quote = "")
+                   quote = "") %>% 
+  select(ident_cely, organizace, hlavni_typ, pristupnost, datum_ukonceni_v)
 
 # vazba <- read_delim(here("data/raw", "export_2021-07-30_vazba_projekt_akce.csv"), 
 #                     delim = "#", 
 #                     col_types = paste0(rep("c", 3), collapse = ""), 
 #                     quote = "")
 
-pian <- read_delim(here("data/raw", "export_2021-07-30_pian.csv"), 
+pian <- read_delim(here("data/raw", 
+                        paste0("export_", datestamp, "_pian.csv")), 
                    delim = "#", 
                    col_types = paste0(rep("c", 13), collapse = ""), 
-                   quote = "")
+                   quote = "") %>% 
+  select(ident_cely, geom_wkt, starts_with("centroid"))
 
-dj <- read_delim(here("data/raw", "export_2021-07-30_dokumentacni_jednotka.csv"), 
+dj <- read_delim(here("data/raw", 
+                      paste0("export_", datestamp, "_dokumentacni_jednotka.csv")), 
                  delim = "#", 
                  col_types = paste0(rep("c", 7), collapse = ""), 
-                 quote = "")
+                 quote = "") %>% 
+  select(ident_cely, parent, pian, negativni_jednotka)
 
 # komp <- read_delim(here("data/raw", "export_2020-09-12_komponenta.csv"), 
 #                    delim = "#", 
 #                    col_types = paste0(rep("c", 19), collapse = ""), 
-#                    quote = "")
-
-
-# select neccessary columns -----------------------------------------------
-
-proj <- proj %>% select(ident_cely, id_rok, stav_popis, typ_projektu, 
-                        organizace_prihlaseni, starts_with("geometry"))
-akce <- akce %>% select(ident_cely, organizace, hlavni_typ, pristupnost, 
-                        datum_ukonceni_v)
-pian <- pian %>% select(ident_cely, geom_wkt, starts_with("centroid"))
-dj <- dj %>% select(ident_cely, parent, pian, negativni_jednotka)
-# komp <- komp %>% select(ident_cely, parent, obdobi, obdobi_poradi, areal)
+#                    quote = "") %>% 
+# select(ident_cely, parent, obdobi, obdobi_poradi, areal)
 
 
 # filtering ---------------------------------------------------------------
